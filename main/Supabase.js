1cb2460f7ab0d752343528233067145c4b8a85e8
waronja <crazsich@gmail.com>
2023-10-17 08:23:59 +0300

    superbase config

--- main/Supabase.js
+++ main/Supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_KEY';
+const supabaseUrl = 'https://ccabmrvwxjhaqqayvqov.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjYWJtcnZ3eGpoYXFxYXl2cW92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc1MDg1ODAsImV4cCI6MjAxMzA4NDU4MH0.KG6GCbX1dj243B838ByyYq7swoMxc1fomrvK_5248uw';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
