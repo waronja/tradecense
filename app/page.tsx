4b1a150cbdd0293b7fb0c1ab40ad41b6337df74c
waronja <crazsich@gmail.com>
2024-11-15 22:22:21 +0300

    Image change and copy

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className="md:h-full h-80 bg-[#FCD0A1] relative overflow-hidden">
           <Image
-            src="/mobile-app.png"
+            src="/Licensor dashboard trademarks.png"
             alt="Mobile App Screenshots"
             fill
             className="object-contain mt-8 md:mt-24 px-14 object-bottom"
@@ -19,10 +19,10 @@ export default function Home() {
 
         <main className="flex flex-col gap-8 mt-8 justify-center px-6 pb-10">
           <h1 className="font-semibold tracking-tight text-zinc-900 text-3xl leading-tight md:text-4xl max-w-lg">
-            Join the conversation: Secure your spot on our Chat App!
+            Acess Trademarks License Within Hours!
           </h1>
           <p className="text-gray-500">
-            Join the waitlist to be notified when our app is available!
+            Join the waitlist and be notified when we go live!
           </p>
 
           <EmailForm />
--- components/EmailFom.tsx
+++ components/EmailFom.tsx
@@ -54,7 +54,7 @@ export default function EmailForm() {
             onChange={handleEmailChange}
           />
           <button
-            className="flex h-10 shrink-0 items-center justify-center gap-1 rounded-lg bg-[#000F2D] px-4 py-2 text-sm font-semibold text-white transition-all hover:bg-zinc-700"
+            className="flex h-10 shrink-0 items-center justify-center gap-1 rounded-lg bg-[#45bb46] px-4 py-2 text-sm font-semibold text-white transition-all hover:bg-zinc-700"
             type="submit"
           >
             <span>Join the waitlist</span>
